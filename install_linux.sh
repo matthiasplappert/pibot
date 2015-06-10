a142b918287e1ac4351cfc508e9de04267ceb0ca
Matthias Plappert <matthiasplappert@me.com>
2015-06-10 18:59:31 +0200

    Update installation scripts

--- install_linux.sh
+++ install_linux.sh
@@ -1,21 +1,9 @@
 # Install dependencies
 sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev wget
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-
-# Download OpenCV 2
-cd /tmp
-wget -O opencv2.zip https://github.com/Itseez/opencv/archive/2.4.11.zip
-unzip opencv2.zip
-cd openvc2
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE ..
-make
-sudo make install
+sudo apt-get install libopencv-dev python-opencv
 
 # Download pip
+source bin/activate
 pip install pip --upgrade
 pip install -r requirements.txt
-
+ln -s /usr/lib/pymodules/python2.7/cv2.so lib/python2.7/cv2.so
--- install_osx.sh
+++ install_osx.sh
@@ -1,9 +1,9 @@
+# Install dependencies
 brew install update
 brew install homebrew/science/opencv
 
-virtualenv ./
+# Set up environment
 source bin/activate
 pip install pip --upgrade
 pip install -r requirements.txt
 ln -s /usr/local/lib/python2.7/site-packages/cv2.so lib/python2.7/cv2.so
-
