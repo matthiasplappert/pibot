9e5b04adabadca14e1e2426ffab9898c2716fb9e
Matthias Plappert <matthiasplappert@me.com>
2015-07-25 18:23:25 +0200

    Fix bugs

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -2,9 +2,10 @@ import freenect
 import timeit
 
 n_iterations = 1000
+print('performing %d iterations ...' % n_iterations)
 start = timeit.default_timer()
-for _ in xrange(n_iterations)
+for _ in xrange(n_iterations):
 	freenect.sync_get_depth()
 duration = timeit.default_timer() - start
 fps = int(float(n_iterations) / duration)
-print('performed %d iterations: total time: %fs (%dfps)' % (n_iterations, duration, fps))
+print('total time: %fs (%dfps)' % (duration, fps))
