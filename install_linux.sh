fe14c0ea0d1bf7aa0d77adf959c90a056dd8bcf5
Matthias Plappert <matthiasplappert@me.com>
2015-07-26 16:15:53 +0200

    Remove outdated scripts

--- install_linux.sh
+++
@@ -1,9 +0,0 @@
-# Install dependencies
-sudo apt-get update
-sudo apt-get install libopencv-dev python-opencv
-
-# Download pip
-source bin/activate
-pip install pip --upgrade
-pip install -r requirements.txt
-ln -s /usr/lib/pymodules/python2.7/cv2.so lib/python2.7/cv2.so
--- install_osx.sh
+++
@@ -1,9 +0,0 @@
-# Install dependencies
-brew install update
-brew install homebrew/science/opencv
-
-# Set up environment
-source bin/activate
-pip install pip --upgrade
-pip install -r requirements.txt
-ln -s /usr/local/lib/python2.7/site-packages/cv2.so lib/python2.7/cv2.so
