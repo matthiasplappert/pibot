67ad0df1f313c620299545d9471ec558b464df5f
Matthias Plappert <matthiasplappert@me.com>
2015-08-02 11:43:55 +0200

    Move interactive to tools

--- src/tools/interactive.py
+++ src/tools/interactive.py
@@ -6,7 +6,6 @@ import curses
 
 import cv2
 
-from util import Action
 from game import GameEnvironment
 
 
@@ -27,18 +26,19 @@ def main(args):
         except:
             key = ''
 
-        if key == 'KEY_UP':
-            action = Action.FORWARD
-        elif key == 'KEY_DOWN':
-            action = Action.BACKWARD
-        elif key == 'KEY_LEFT':
-            action = Action.TURN_LEFT
-        elif key == 'KEY_RIGHT':
-            action = Action.TURN_RIGHT
-        elif key.upper() == 'X':
-            break
-        else:
-            action = Action.IDLE
+        # if key == 'KEY_UP':
+        #     action = Action.FORWARD
+        # elif key == 'KEY_DOWN':
+        #     action = Action.BACKWARD
+        # elif key == 'KEY_LEFT':
+        #     action = Action.TURN_LEFT
+        # elif key == 'KEY_RIGHT':
+        #     action = Action.TURN_RIGHT
+        # elif key.upper() == 'X':
+        #     break
+        # else:
+        #     action = Action.IDLE
+        action = 0
 
         start = timeit.default_timer()
         frame, reward, terminal, lives, processed_frame, light = game.debug_step(action)
@@ -70,7 +70,7 @@ def get_parser():
     parser.add_argument('--host', help='host of the robot, e.g. 192.168.1.2', type=str, default=None)
     parser.add_argument('--port', help='port of the robot, e.g. 9090', type=int, default=9090)
     parser.add_argument('--interval', help='update interval in seconds', type=int, default=0)
-    parser.add_argument('agent', help='name of the agent')
+    parser.add_argument('agent', help='name of the robot')
     return parser
 
 
